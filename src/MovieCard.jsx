c3ea7ee9780cbd46230f4c5fd091c790d0094dd7
robin8778 <robin.007660@gmail.com>
2023-09-17 19:42:24 +0530

    Final Changes

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,20 +2,18 @@ import React from 'react'
 
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
     return (
-        <>
-            <div className="movie" key={imdbID}>
-                <div>
-                    <p>{Year}</p>
-                </div>
-                <div>
-                    <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
-                </div>
-                <div>
-                    <span>{Type}</span>
-                    <h3>{Title}</h3>
-                </div>
+        <div className="movie" key={imdbID}>
+            <div>
+                <p>{Year}</p>
             </div>
-        </>
+            <div>
+                <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+            </div>
+            <div>
+                <span>{Type}</span>
+                <h3>{Title}</h3>
+            </div>
+        </div>
     )
 }
 
